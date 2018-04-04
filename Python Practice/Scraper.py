92af0c023d802c9e49cb1a644a141132bc5d4f06
Mutedmelodys <mutedmelodys@gmail.com>
2018-04-03 23:44:26 -0400

    Addition

--- Python Practice/Scraper.py
+++ Python Practice/Scraper.py
@@ -2,8 +2,6 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 html = urlopen("https://www.reddit.com/r/news/")
 bsObj = BeautifulSoup(html.read());
-print(bsObj.p)
+print(bsObj)
 
 
-for link in bsObj.find_all('a'):
-    print(link.get('href'))
